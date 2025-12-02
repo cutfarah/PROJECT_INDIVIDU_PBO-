0356b60e3e378c97187467c9c63c1c32f660fdca
cutfarah <cutfarahsalsabila76@gmail.com>
2025-12-02 16:27:54 +0700

    Create Bus.java

---
+++ Bus.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Bus {
+
+    private List<Penumpang> penumpangBiasa;
+    private List<Penumpang> penumpangPrioritas;
+    private List<Penumpang> penumpangBerdiri;
