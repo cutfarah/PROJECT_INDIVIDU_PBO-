d174031e1ef1c2792c5d51047c1c45975a802166
cutfarah <cutfarahsalsabila76@gmail.com>
2025-12-02 16:38:14 +0700

    Update TestBus.java

--- TestBus.java
+++ TestBus.java
@@ -7,3 +7,22 @@ public class TestBus {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Bus bus = new Bus();
+
+      boolean jalan = true;
+        while (jalan) {
+            System.out.println("===== BUS TRANS KOETARADJA ======");
+            System.out.println("MENU : ");
+            System.out.println("1. Naikkan Penumpang");
+            System.out.println("2. Turunkan Penumpang");
+            System.out.println("3. Lihat Penumpang");
+            System.out.println("4. Keluar");
+            System.out.print("Pilihan : ");
+
+            String input = in.nextLine();
+            int pilihan;
+            try {
+                pilihan = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Input tidak valid!\n");
+                continue;
+            }
