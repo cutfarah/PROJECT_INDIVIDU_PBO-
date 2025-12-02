6f609744b7ce2493b35f8bd195c3a3f685ef9c0c
cutfarah <cutfarahsalsabila76@gmail.com>
2025-12-02 16:20:14 +0700

    Create Penumpang.java

---
+++ Penumpang.java
@@ -0,0 +1,7 @@
+public class Penumpang extends Orang implements PembayarBus {
+
+    private int id;
+    private int umur;
+    private boolean hamil;
+    private int saldo; // dalam rupiah
+    private KategoriPenumpang kategori;
