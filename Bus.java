bdf459d54794f8c58a3c9318a8347517d717df35
cutfarah <cutfarahsalsabila76@gmail.com>
2025-12-02 16:28:29 +0700

    Update Bus.java

--- Bus.java
+++ Bus.java
@@ -7,3 +7,12 @@ public class Bus {
     private List<Penumpang> penumpangBiasa;
     private List<Penumpang> penumpangPrioritas;
     private List<Penumpang> penumpangBerdiri;
+
+  public static final int ONGKOS_BUS = 2000;
+
+    private static final int MAKS_KURSI_BIASA = 16;
+    private static final int MAKS_KURSI_PRIORITAS = 4;
+    private static final int MAKS_BERDIRI = 20;
+    private static final int MAKS_TOTAL = 40;
+
+    private int totalPendapatan;
