a1912ed6ec2fc5769ce5c3ca46ff0a6f5a70ef55
cutfarah <cutfarahsalsabila76@gmail.com>
2025-12-02 16:37:41 +0700

    Create TestBus.java

---
+++ TestBus.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TestBus {
+
+    private static int NEXT_ID = 1;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Bus bus = new Bus();
